a49ad740332a8a9c2c7c62beccb705df1f73d6f7
Arkadip <in2arkadipb13@gmail.com>
2021-03-24 14:59:47 +0530

    fix: Covered the whole validation shuff under joi.object

--- backend/src/api/validations/auth.validation.js
+++ backend/src/api/validations/auth.validation.js
@@ -1,9 +1,9 @@
-const joi = require('joi')
+const Joi = require('joi')
 
 module.exports = {
     // POST /api/auth/register
     register: {
-        body: {
+        body: Joi.object({
             email: Joi.string()
                 .email()
                 .required(),
@@ -11,18 +11,18 @@ module.exports = {
                 .required()
                 .min(6)
                 .max(128),
-        },
+        }),
     },
 
     // POST /api/auth/login
     login: {
-        body: {
+        body: Joi.object({
             email: Joi.string()
                 .email()
                 .required(),
             password: Joi.string()
                 .required()
                 .max(128),
-        },
+        })
     },
 }
