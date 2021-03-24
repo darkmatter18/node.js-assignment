70b8de62296d6342a6377610726c259a2dca15c2
Arkadip <in2arkadipb13@gmail.com>
2021-03-24 12:22:30 +0530

    Added dotenv_safe for get .env  and other vars
    vars added: mongo.uri, env, port

--- backend/src/config/vars.js
+++ backend/src/config/vars.js
@@ -1,4 +1,18 @@
+const path = require('path');
+
+// import .env variables
+require('dotenv-safe').config({
+  path: path.join(__dirname, '../../../.env'),
+  sample: path.join(__dirname, './../../../.env.example'),
+});
+
+
 module.exports = {
+    env: process.env.NODE_ENV,
     jwtSecret: process.env.JWT_SECRET,
-    logs: process.env.NODE_ENV === 'production' ? 'combined' : 'dev'
+    logs: process.env.NODE_ENV === 'production' ? 'combined' : 'dev',
+    mongo: {
+        uri: process.env.NODE_ENV === 'test' ? process.env.MONGO_URI_TESTS : process.env.MONGO_URI,
+    },
+    port: process.env.PORT || 8080
 }
