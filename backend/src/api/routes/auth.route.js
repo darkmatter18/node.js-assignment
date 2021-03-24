73a6c4ec40d142fcffd7a13820bef60c9bfd2458
Arkadip <in2arkadipb13@gmail.com>
2021-03-24 15:03:35 +0530

    some more boilerplate added in auth.route

--- backend/src/api/routes/auth.route.js
+++ backend/src/api/routes/auth.route.js
@@ -63,4 +63,28 @@ router.route('/register').post(validate(register), (req, res) => res.send("DONE"
  */
 router.route('/login').post(validate(login))
 
+/**
+ * @api {post} v1/auth/refresh-token
+ * @apiDescription Refresh expired accessToken
+ * @apiName RefreshToken
+ * @apiGroup Auth
+ * @apiPermission public
+ *
+ * @apiParam  {String}  email         User's email
+ * @apiParam  {String}  refreshToken  Refresh token aquired when user logged in
+ *
+ * @apiSuccess {String}  tokenType     Access Token's type
+ * @apiSuccess {String}  accessToken   Authorization Token
+ * @apiSuccess {String}  refreshToken  Token to get a new accessToken after expiration time
+ * @apiSuccess {Number}  expiresIn     Access Token's expiration time in miliseconds
+ *
+ * @apiError (Bad Request 400)  ValidationError  Some parameters may contain invalid values
+ * @apiError (Unauthorized 401)  Unauthorized     Incorrect email or refreshToken
+ */
+ router.route('/refresh-token').post();
+
+router.route('/send-password-reset').post();
+
+router.route('/reset-password').post();
+
 module.exports = router
