483fbe155ccfbee93b78d488de01101fcb9c1012
Arkadip <in2arkadipb13@gmail.com>
2021-03-24 12:23:02 +0530

    refactor: index as per new env vars

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,4 @@
-const { port, env } = require('./config/vars');
+const vars = require('./config/vars');
 const logger = require('./config/logger');
 const app = require('./config/express');
 const mongoose = require('./config/mongoose');
@@ -7,7 +7,7 @@ const mongoose = require('./config/mongoose');
 mongoose.connect();
 
 // listen to requests
-app.listen(port, () => logger.info(`server started on port ${port} (${env})`));
+app.listen(vars.port, () => logger.info(`server started on port ${vars.port} (${vars.env})`));
 
 /**
 * Exports express
