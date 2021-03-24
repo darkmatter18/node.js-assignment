ae0aad9075307245aa58a4947edf00baca5328cb
Arkadip <in2arkadipb13@gmail.com>
2021-03-24 20:52:31 +0530

    Added register functionity

---
+++ backend/src/api/controllers/auth.controller.js
@@ -0,0 +1,42 @@
+const { omit } = require('lodash');
+const moment = require('moment');
+const httpStatus = require('http-status');
+
+const User = require('../models/user.model');
+const RefreshToken = require('../models/refreshToken.model');
+
+const vars = require('./../../config/vars');
+
+/**
+ * Returns a formated object with tokens
+ * @private
+ */
+function generateTokenResponse(user, accessToken) {
+    const tokenType = 'Bearer';
+    const refreshToken = RefreshToken.generate(user).token;
+    const expiresIn = moment().add(vars.jwtExpirationInterval, 'minutes');
+    return {
+        tokenType,
+        accessToken,
+        refreshToken,
+        expiresIn,
+    };
+}
+
+/**
+ * @api {POST} /api/auth/register
+ * Returns jwt token if registration was successful
+ * @public
+ */
+exports.register = async (req, res, next) => {
+    try {
+        const userData = omit(req.body, 'role');
+        const user = await new User(userData).save();
+        const userTransformed = user.transform();
+        const token = generateTokenResponse(user, user.token());
+        res.status(httpStatus.CREATED);
+        return res.json({ token, user: userTransformed });
+    } catch (error) {
+        return next(User.checkDuplicateEmail(error));
+    }
+};
