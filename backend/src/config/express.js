521191cf76824ecb4457e50d23ad4bb74e6f29b5
Arkadip <in2arkadipb13@gmail.com>
2021-03-26 13:41:56 +0530

    CSP policies added

--- backend/src/config/express.js
+++ backend/src/config/express.js
@@ -12,6 +12,9 @@ const helmet = require('helmet');
 const cors = require('cors');
 const passport = require('passport')
 const passportStategies = require('./passport')
+const path = require('path');
+const { expressCspHeader, INLINE, NONE, SELF } = require('express-csp-header');
+
 
 const vars = require('./vars');
 const router = require('../api/routes');
@@ -29,7 +32,7 @@ app.use(morgan(vars.logs));
 
 // Parse request body params and attach them to req.body
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
 // Compress the response in gzip for better performance
 app.use(compression());
@@ -41,6 +44,20 @@ app.use(methodOverride());
 // secure apps by setting various HTTP headers
 app.use(helmet());
 
+app.use(expressCspHeader({
+    directives: {
+        'connect-src': [SELF, INLINE, 'https://newsapi.org'],
+        'default-src': [SELF],
+        'font-src': [SELF, 'https://fonts.gstatic.com',  'https://fonts.googleapis.com'],
+        'script-src': [SELF, INLINE, 'somehost.com'],
+        'style-src': [SELF, INLINE, 'https://fonts.googleapis.com'],
+        'img-src': [SELF, 'https://source.unsplash.com', 'https://images.unsplash.com'],
+        'worker-src': [NONE],
+        'block-all-mixed-content': true
+    }
+}));
+
+
 // Using Cross Origin Resource Sharing for APIs
 app.use(cors());
 
@@ -49,8 +66,11 @@ app.use(passport.initialize());
 passport.use('jwt', passportStategies.jwt);
 
 // Add the api router
+app.use(express.static(path.join(__dirname, '../client/build')));
 app.use('/api', router);
-
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '../client/build/index.html'));
+});
 
 app.use(errors.converter);
 app.use(errors.notFound);
