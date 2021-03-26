e1681684f0603d2031406d0909ce7e41cae127c8
Arkadip <in2arkadipb13@gmail.com>
2021-03-26 12:25:10 +0530

    All bugs solved

--- frontend/src/Components/NewBlogComponent/index.js
+++ frontend/src/Components/NewBlogComponent/index.js
@@ -37,7 +37,7 @@ const NewBlogComponent = () => {
     const onSubmitEvent = async (data) => {
         const {success, err} =  await api({...data, userEmail: authUser().email}, auth())
         if (err && !success) {
-            if (err.response.data.message){
+            if (err.response){
                 alert(err.response.data.message)
             }  else {
                 alert(err);
