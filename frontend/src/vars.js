01d037c0e5d5c124a107f66673d9d48343217c94
Arkadip <in2arkadipb13@gmail.com>
2021-03-26 17:27:20 +0530

    Updated every thing

--- frontend/src/vars.js
+++ frontend/src/vars.js
@@ -1,4 +1,4 @@
-export const API_baseURL = "http://localhost:3000/api"
+export const API_baseURL = process.env.NODE_ENV === "production" ? "/api" : "http://localhost:3000/api"
 export const API_ROUTE_LOGIN = "/auth/login"
 export const API_ROUTE_REGISTER = "/auth/register"
 export const API_ROUTE_FORGOT_PASSWORD = "/auth/send-password-reset"
