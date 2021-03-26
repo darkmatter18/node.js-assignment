90010d9cbe531713c72a7a395963ad4e9be7ea15
Arkadip <in2arkadipb13@gmail.com>
2021-03-26 11:01:32 +0530

    validation for posting a blog is created

---
+++ backend/src/api/validations/user.validation.js
@@ -0,0 +1,17 @@
+const Joi = require('joi')
+
+module.exports = {
+    // POST /api/auth/register
+    postBlog: {
+        body: Joi.object({
+            title: Joi.string()
+                .min(10)
+                .required(),
+            body: Joi.string()
+                .required(),
+            userEmail: Joi.string()
+                .email()
+                .required()
+        }),
+    }
+}
