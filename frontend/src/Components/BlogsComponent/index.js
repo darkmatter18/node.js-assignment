7308aee40ade02779a11abaa7c1059a3e55f6ece
Arkadip <in2arkadipb13@gmail.com>
2021-03-26 10:01:06 +0530

    BLogs and New blog component boilerplate added

---
+++ frontend/src/Components/BlogsComponent/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+import AppBarComponent from "../AppBarComponent"
+
+const BlogsComponent = () => {
+    return (
+        <React.Fragment>
+            <AppBarComponent/>
+        </React.Fragment>
+    )
+}
+
+export default BlogsComponent
---
+++ frontend/src/Components/NewBlogComponent/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+import AppBarComponent from "../AppBarComponent"
+
+const NewBlogComponent = () => {
+    return (
+        <React.Fragment>
+            <AppBarComponent/>
+        </React.Fragment>
+    )
+}
+
+export default NewBlogComponent
