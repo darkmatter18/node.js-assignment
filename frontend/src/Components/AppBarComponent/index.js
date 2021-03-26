c297cdc879a1ff4003c219c504a331ba6ea5810d
Arkadip <in2arkadipb13@gmail.com>
2021-03-26 10:00:43 +0530

    Alll links in Appbar added

--- frontend/src/Components/AppBarComponent/index.js
+++ frontend/src/Components/AppBarComponent/index.js
@@ -1,6 +1,7 @@
 import { AppBar, Button, Toolbar, Typography, makeStyles } from "@material-ui/core"
 import { useAuthUser, useSignOut } from "react-auth-kit"
 import { useHistory } from "react-router"
+import { BLOGS, NEW_BLOG, DASHBOARD } from "../RouteComponent/routes"
 
 const useStyles = makeStyles(() => ({
     flex: {
@@ -22,8 +23,9 @@ const AppBarComponent = () => {
                     <Typography variant="h6" color="inherit" noWrap className={classes.flex}>
                         Hello, {authState().name}
                     </Typography>
-                    <Button color="inherit" onClick={() => { history.push("#") }}>New Blog</Button>
-                    <Button color="inherit" onClick={() => { history.push("#") }}>My Blogs</Button>
+                    <Button color="inherit" onClick={() => { history.push(DASHBOARD) }}>Dashboard</Button>
+                    <Button color="inherit" onClick={() => { history.push(NEW_BLOG) }}>New Blog</Button>
+                    <Button color="inherit" onClick={() => { history.push(BLOGS) }}>My Blogs</Button>
                     <Button color="inherit" onClick={() => { signOut() }}>Logout</Button>
                 </Toolbar>
             </AppBar>
